9f36e4d9824ac90d4877975ba47c8badf89ebf89
Takuro Inada <107355417+mentaco@users.noreply.github.com>
2024-05-06 01:49:52 +0900

    Split functions into another file

---
+++ move_files.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -euo pipefail
+
+BACKUP_DIR="$1"
+PATH_RECORD="$2"
+shift
+shift
+
+FILES="$@"
+
+function mk_backup_dir() {
+    if [ ! -d "${BACKUP_DIR}" ]; then
+        mkdir "${BACKUP_DIR}"
+        echo "Make directory at "$(realpath "${BACKUP_DIR}")""
+    fi
+}
+
+function record_path() {
+    local path="$(realpath "$1" | sed "s|$HOME|~|")"
+    local element="${path} $2"
+    echo "${element}" >> "${PATH_RECORD}"
+}
+
+function move_file() {
+    local date="$(date +%y%m%d_%R:%S)"
+
+    for item; do
+        if [ "${item}" = "--" ]; then
+            continue
+        fi
+
+        local file="$(basename -- "${item}")"
+        local backup_file="${BACKUP_DIR}/${file}_${date}"
+        record_path "${item}" "${backup_file}"
+        mv -- "${item}" "${backup_file}"
+    done
+}
+
+mk_backup_dir
+move_file "${FILES[@]}"
--- srm.sh
+++ srm.sh
@@ -24,9 +24,11 @@ if [ "$#" -le 0 ]; then
     echo "Error: Missing arguments."
     show_help
     exit 1
+
 elif [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
     show_help
     exit 0
+
 elif [ "$1" = "start" ]; then
     cd "$(dirname -- "$(realpath "$0")")"
     if [ -f "${NOHUP_PID_FILE}" ]; then
@@ -37,6 +39,7 @@ elif [ "$1" = "start" ]; then
     nohup ./delete_old_files.sh "${BACKUP_DIR}" "${PATH_RECORD}" > /dev/null &
     echo "$!" > "${NOHUP_PID_FILE}"
     exit 0
+
 elif [ "$1" = "stop" ]; then
     cd "$(dirname -- "$(realpath "$0")")"
     if [ ! -f "${NOHUP_PID_FILE}" ]; then
@@ -50,52 +53,25 @@ elif [ "$1" = "stop" ]; then
     rm "${NOHUP_PID_FILE}"
     echo "Stoped process."
     exit 0
+
 elif [ "$1" = "list" ]; then
     cd "$(dirname -- "$(realpath "$0")")"
     ./show_list.sh "${BACKUP_DIR}" "${PATH_RECORD}"
     exit 0
+
 elif [ "$1" = "restore" ]; then
     shift
     cd "$(dirname -- "$(realpath "$0")")"
     ./restore_files.sh "${BACKUP_DIR}" "${PATH_RECORD}" "$@"
     exit 0
-fi
 
-
-function mk_backup_dir() {
-    if [ ! -d "${BACKUP_DIR}" ]; then
-        mkdir "${BACKUP_DIR}"
-        echo "Make directory at "$(realpath "${BACKUP_DIR}")""
-    fi
-}
-
-function record_path() {
-    local path="$(realpath "$1" | sed "s|$HOME|~|")"
-    local element="${path} $2"
-    echo "${element}" >> "${PATH_RECORD}"
-}
-
-function move_file() {
-    local date="$(date +%y%m%d_%R:%S)"
-
+else
+    FILES=()
     for item; do
-        if [ "${item}" = "--" ]; then
-            continue
-        fi
-
-        local file="$(basename -- "${item}")"
-        local backup_file="${BACKUP_DIR}/${file}_${date}"
-        record_path "${item}" "${backup_file}"
-        mv -- "${item}" "${backup_file}"
+        FILES+=("$(realpath "${item}")")
     done
-}
 
-FILES=()
-for item; do
-    FILES+=("$(realpath "${item}")")
-done
-
-cd "$(dirname -- "$(realpath "$0")")"
-
-mk_backup_dir
-move_file "${FILES[@]}"
+    cd "$(dirname -- "$(realpath "$0")")"
+    ./move_files.sh "${BACKUP_DIR}" "${PATH_RECORD}" "${FILES[@]}"
+    exit 0
+fi
